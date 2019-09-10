5c3c774c14f72b0f0c40080f3fcd678f93c500b6
Manuel <manuelm866@gmail.com>
2019-09-10 10:05:18 -0300

    Scroll host listener instead of view check for sticky header

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit, ElementRef, ViewChild, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit, AfterViewChecked {
+export class HeaderComponent implements OnInit {
+  @ViewChild('authorLine', { static: false }) authorView!: ElementRef;
+  @ViewChild('header', { static: false }) headerView!: ElementRef;
 
-  @ViewChild('authorLine', { static: false }) authorView !: ElementRef;
-  @ViewChild('header', { static: false }) headerView !: ElementRef;
-
-  constructor() {}
-
-  ngOnInit() {}
-
-  ngAfterViewChecked() {
-    setTimeout(() => {
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    if (this.authorView && this.headerView) {
       const element = this.authorView.nativeElement as HTMLDivElement;
       const headerElement = this.headerView.nativeElement as HTMLDivElement;
       const currentOffset = headerElement.clientHeight - element.clientHeight - 2;
       if (window.pageYOffset > currentOffset) {
-        element.classList.add("sticky");
+        element.classList.add('sticky');
       } else {
-        element.classList.remove("sticky");
+        element.classList.remove('sticky');
       }
-    });
+    }
   }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
